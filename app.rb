04d83ea34c81c4f198d5042a74a620bcedb54de8
wakelank <wakelank@gmail.com>
2014-07-03 15:48:11 -0400

    Cities stored in DB, displayed on index and show pages.

--- app.rb
+++ app.rb
@@ -1,2 +1,28 @@
-class City < ActiveRecord::Base
+require 'bundler'
+Bundler.require
+
+require_relative 'models/city'
+require_relative './config'
+
+get '/' do
+  redirect "/cities"
+end
+
+get '/cities' do 
+  @cities = City.all 
+  erb :index
+end
+
+get '/cities/new' do
+  erb :new
+end
+
+post '/cities' do
+  city = City.create(name: params["name"])
+  redirect "/cities/#{ city.id }"
+end
+
+get '/cities/:id' do
+  @city = City.find(params[:id])
+  erb :show
 end
