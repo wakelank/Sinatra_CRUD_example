f32f1bde93a79ccaf81f3cdd32cde09e1801932f
wakelank <wakelank@gmail.com>
2014-07-03 16:52:40 -0400

    Minor styling.  Some bug fixes

--- app.rb
+++ app.rb
@@ -4,34 +4,41 @@ Bundler.require
 require_relative 'models/city'
 require_relative './config'
 
+#index
 get '/' do
   redirect "/cities"
 end
 
+#show all
 get '/cities' do 
   @cities = City.all 
   erb :index
 end
 
+# new
 get '/cities/new' do
   erb :new
 end
 
+# create
 post '/cities' do
   city = City.create(name: params["name"])
   redirect "/cities/#{ city.id }"
 end
 
+#show
 get '/cities/:id' do
   @city = City.find(params[:id])
   erb :show
 end
 
+#edit
 get '/cities/:id/edit' do
   @city = City.find(params[:id])
   erb :edit
 end
 
+#update
 put '/cities/:id' do
   city = City.find(params[:id])
   city.name = params[:name]
@@ -39,6 +46,7 @@ put '/cities/:id' do
   redirect "/cities/#{ city.id }"
 end
 
+#delete
 delete '/cities/:id' do
   City.delete(params[:id])
   redirect '/cities'
